472df2911a437cc1671e9f7301b64a352ba89a88
masukomi <masukomi@masukomi.org>
2019-11-23 22:27:54 -0500

    added eggs to install_chicken_eggs.sh

--- src/install_chicken_eggs.sh
+++ src/install_chicken_eggs.sh
@@ -2,15 +2,23 @@
 
 # makes sure all the required chicken scheme eggs
 # are installed
-echo "Will run chicken-install for required eggs"
+echo "Will run chicken-install for required eggs (libraries)"
 
+
+chicken-install args
 chicken-install filepath
+chicken-install format
+chicken-install http-client
 chicken-install intarweb
+chicken-install linenoise
 chicken-install medea
+chicken-install message-digest
+chicken-install sha2
+chicken-install shell
+chicken-install simple-exceptions
 chicken-install simple-loops
 chicken-install spiffy
 chicken-install spiffy-request-vars
 chicken-install uri-common
-chicken-install shell
 
 echo "DONE"
